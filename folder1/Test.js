471b8ba5f82f2a43baba18c6f77e275bbda06137
AndriiSavchuk1989 <andrey.m.savchuk@gmail.com>
2024-02-10 18:51:08 +0200

    Add functions array

--- folder1/Test.js
+++ folder1/Test.js
@@ -14,3 +14,22 @@ let sum = val => {
 };
 
 console.log(sum(1)(2)(3)(4)(5)(6)(7).toString())
+
+let arr = [];
+let items = 0;
+
+while(items < 5) {
+    let func = items => {
+        let indexToShow = items;
+        return () => indexToShow;
+    }
+
+    arr.push(func(items));
+    items++;
+};
+
+console.log(arr[0]())
+console.log(arr[1]())
+console.log(arr[2]())
+console.log(arr[3]())
+console.log(arr[4]())
